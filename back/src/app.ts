5b67023a5a13fc2e213419f711c5bf69646e6195
Kiss-Shot-Acerola-Heart-Under-Blade <johan.chevet@hotmail.fr>
2023-10-11 18:07:17 +0200

    started implementing socket

--- back/src/app.ts
+++ back/src/app.ts
@@ -2,17 +2,24 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import errorHandler from "./Utils/errorHandler";
+import http from "http";
+import {Server} from "socket.io";
 
 class App {
   public app: express.Application;
   public port: number;
+  public io: Server;
+  private server: http.Server;
 
   constructor(controllers: any, port: number) {
     this.app = express();
     this.port = port;
+    this.server = http.createServer(this.app);
+    this.io = new Server(this.server);
 
     this.initializeMiddlewares();
     this.initializeControllers(controllers);
+    this.initializeWebSocket();
     this.app.use(errorHandler);
   }
 
@@ -38,8 +45,27 @@ class App {
     });
   }
 
+  private initializeWebSocket() {
+    this.io.use;
+    console.log("initializeWebSocket");
+    this.io.on("connection", (socket) => {
+      console.log("user " + socket.id + " connected");
+      socket.send("hello");
+      socket.on("message", function message(data) {
+        console.log("received: %s", data);
+      });
+      socket.on("disconnect", () => {
+        console.log("user " + socket.id + " disconnected");
+      });
+    });
+  }
+
+  public getIO(): Server {
+    return this.io;
+  }
+
   public listen() {
-    this.app.listen(this.port, () => {
+    this.server.listen(this.port, () => {
       console.log(`App listening on the port ${this.port}`);
     });
   }
