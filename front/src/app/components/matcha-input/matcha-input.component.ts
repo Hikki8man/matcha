7f9d0e85b38859fb965d1e3c79ab8efde653cd07
unknown <lbertran@student.42lyon.fr>
2023-11-01 17:38:23 +0100

    fix: fix login inputs

--- front/src/app/components/matcha-input/matcha-input.component.ts
+++ front/src/app/components/matcha-input/matcha-input.component.ts
@@ -7,8 +7,26 @@ import { FormControl, FormGroup } from '@angular/forms';
     styleUrls: ['./matcha-input.component.scss'],
 })
 export class MatchaInputComponent {
+
     @Input() public Placeholder: string;
     @Input() public Type: string;
-    @Input() public FormGroup: FormGroup;
+    @Input()
+    get FormGroup(): FormGroup {
+        return this._formGroup;
+    }
+    set FormGroup(formGroup: FormGroup) {
+        this._formGroup = formGroup;
+        this.init();
+    }
     @Input() public Name: string;
+    
+    public InputFormControl: FormControl = new FormControl();
+
+    private _formGroup: FormGroup;
+
+    private init(): void {
+        if (this.FormGroup) {
+            (this.InputFormControl as any) = this.FormGroup.controls[this.Name];
+        }
+    }
 }
