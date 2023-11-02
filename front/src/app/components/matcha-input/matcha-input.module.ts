fc5a500196a6df07c87689cebbe4a75bfcba83bd
wshloic <lbertran@student.42lyon.fr>
2023-11-02 09:59:41 +0100

    feat: add password visibility button

--- front/src/app/components/matcha-input/matcha-input.module.ts
+++ front/src/app/components/matcha-input/matcha-input.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatchaInputComponent } from './matcha-input.component';
+import { SvgIconComponentModule } from '../svg-icon/svg-icon.module';
 
 @NgModule({
     declarations: [MatchaInputComponent],
-    imports: [CommonModule, FormsModule, ReactiveFormsModule],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, SvgIconComponentModule],
     exports: [MatchaInputComponent],
 })
 export class MatchaInputModule {}
