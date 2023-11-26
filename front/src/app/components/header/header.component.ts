1014bf227d3d56dc207368e6538bbd94440be855
wshloic <lbertran@student.42lyon.fr>
2023-11-26 18:24:00 +0100

    feat: display real avatar on header

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { IconUrlEnum } from 'src/app/enums/icon-url-enum';
+import { IAuthenticationService } from 'src/app/services/authentication/iauthentication.service';
+import { IProfileService } from 'src/app/services/profile/iprofile.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+
     public HeartIconUrl: string = IconUrlEnum.Heart;
     public HeartIconStyle: Record<string, string> = {
         display: 'flex',
         height: '14px',
     };
-    public AvatarUrl: string = 'assets/images/becoshy.png';
+    public AvatarUrl: string = null;
+
+    private _subscriptions: Subscription[] = [];
+
+    constructor(
+        private readonly _authenticationService: IAuthenticationService,
+        private readonly _profileService: IProfileService,
+    ) { }
+
+    ngOnInit(): void {
+        const userId = this._authenticationService.profileValue.id;
+        this._subscriptions.push(this._profileService.getAvatar(userId).subscribe((avatar) => {
+            this.AvatarUrl = avatar;
+        }));
+    }
+
+    ngOnDestroy(): void {
+        this._subscriptions.forEach((sub) => sub.unsubscribe());
+    }
 }
