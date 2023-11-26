91253954dcccb21ce2b148c6a71ef59be8013951
wshloic <lbertran@student.42lyon.fr>
2023-11-26 18:18:34 +0100

    chore: remove notifications button from header

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -7,22 +7,10 @@ import { IconUrlEnum } from 'src/app/enums/icon-url-enum';
     styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-    public NotificationIconUrl: string = IconUrlEnum.Notification;
-    public NotificationIconStyle: Record<string, string> = {
-        display: 'flex',
-        height: '26px',
-        width: '26px',
-        fill: 'var(--color-pink-accent)',
-    };
     public HeartIconUrl: string = IconUrlEnum.Heart;
     public HeartIconStyle: Record<string, string> = {
         display: 'flex',
         height: '14px',
     };
-    public HasNotifications: boolean = true;
     public AvatarUrl: string = 'assets/images/becoshy.png';
-
-	public showNotifications() {
-		this.HasNotifications = !this.HasNotifications;
-	}
 }
