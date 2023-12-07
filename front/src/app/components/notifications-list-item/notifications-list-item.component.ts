1a8b983dc73c13d0081015bed84a5dc67f8be5c6
wshloic <lbertran@student.42lyon.fr>
2023-12-07 13:48:58 +0100

    feat: c'est twitter ou quoi

--- front/src/app/components/notifications-list-item/notifications-list-item.component.ts
+++ front/src/app/components/notifications-list-item/notifications-list-item.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppPathEnum } from 'src/app/enums/app-path-enum';
 import { IconUrlEnum } from 'src/app/enums/icon-url-enum';
+import { IAuthenticationService } from 'src/app/services/authentication/iauthentication.service';
+import { IProfileService } from 'src/app/services/profile/iprofile.service';
 import { timeAgo } from 'src/app/utils/timeAgo';
 
 
@@ -10,17 +12,26 @@ import { timeAgo } from 'src/app/utils/timeAgo';
 	templateUrl: './notifications-list-item.component.html',
 	styleUrls: ['./notifications-list-item.component.scss']
 })
-export class NotificationsListItemComponent {
+export class NotificationsListItemComponent implements OnInit {
 
 	public HeartIconUrl: string = IconUrlEnum.Heart;
-	public IconStyle: Record<string, string> = { display: 'flex', height: '18px', width: '18px', fill: 'var(--title-color)' };
+	public IconStyle: Record<string, string> = { display: 'flex', height: '24px', width: '24px', fill: 'var(--title-color)' };
 
 	public UserName: string = 'Test user';
 	public Details: string = 'a liké ton profil';
+	public AvatarUrl: string = null;
 
 	constructor(
 		private readonly _router: Router,
+		private readonly _authenticationService: IAuthenticationService,
+		private readonly _profileService: IProfileService,
 	) { }
+
+	ngOnInit(): void {
+		this.AvatarUrl = this._profileService.getAvatar(
+            this._authenticationService.profileValue.avatar,
+        );
+	}
 
 	public redirectToProfile(): void {
 		this._router.navigate([AppPathEnum.Profile, '2']);
